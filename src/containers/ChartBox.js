309c563591cd959145656d580192a4c00b4a5e72
Douglas Russell <douglas.russell7@btinternet.com>
2021-12-01 16:33:42 +0000

    extensions done

--- src/containers/ChartBox.js
+++ src/containers/ChartBox.js
@@ -4,16 +4,34 @@ import ChartList from "../components/ChartList"
 const ChartBox = () =>  {
     const [chart, setChart] = useState([]);
 
-    const getChartData = () => {
-        fetch('https://itunes.apple.com/gb/rss/topsongs/limit=20/json')
+    const getChartData = (url) => {
+        fetch(url)
             .then(response => response.json())
             .then(data => setChart(data.feed.entry))
     }
     
     useEffect(() => getChartData(), []);
 
+    const handleChange = (event) => {
+        const dict= {
+            all: "https://itunes.apple.com/gb/rss/topsongs/limit=20/json",
+            rock: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=21/json",
+            dance: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=17/json",
+            country: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=6/json",
+        }
+        getChartData(dict[event.target.value])
+    }
+
     return(
+        <>
+    <select onChange={handleChange}>
+        <option selected value="all">All</option>
+        <option value="rock">Rock</option>
+        <option value="dance">Dance</option>
+        <option value="country">Country</option>
+    </select>
     <ChartList chart={chart}/>
+    </>
     )
 }
 export default ChartBox;
